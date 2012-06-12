3e0e89b35faa8fdf1290aabcfa221bae68afa69c
Joeri <joeri@youniware.com>
2012-06-12 22:00:57 +0200

    start page content change

--- application/models/Page.php
+++ application/models/Page.php
@@ -31,5 +31,15 @@ class Application_Model_Page extends Zend_Db_Table_Abstract
         $rows = $this->fetchAll($select)->current(); //current maakt dat je maar 1 row hebt. Anders moet je nen foreach gebruiken om over je resultate te lopen.
         return $rows;
     }
+    
+    public function getContent($page){
+        $select = $this->select(Zend_Db_Table::SELECT_WITHOUT_FROM_PART);
+        $select->setIntegrityCheck(false)
+                ->join('pageLocal', 'page.pageID = pageLocal.pageID')
+                ->where('pageLocal.titleUrl = ?', $page);
+        
+        $rows = $this->fetchAll($select)->current(); //current maakt dat je maar 1 row hebt. Anders moet je nen foreach gebruiken om over je resultate te lopen.
+        return $rows;
+    }
 
 }
